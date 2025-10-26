4f16734b91f269ca4553179b832e4bef74385121
eliphinehas-ops <eliphinehas@gmail.com>
2025-10-26 22:34:10 +0530

    Add risk scoring query for financial instruments
    This query assigns risk scores to financial instruments based on their credit rating and volatility.

--- SQL script/risk_score.sql
+++ SQL script/risk_score.sql
@@ -1,1 +1,16 @@
+-- Assign risk scores based on volatility and credit rating
+SELECT
+  InstrumentID,
+  InstrumentType,
+  Issuer,
+  Sector,
+  CreditRating,
+  MarketValue,
+  Volatility,
+  CASE
+    WHEN CreditRating = 'A' AND Volatility < 0.10 THEN 'Low Risk'
+    WHEN CreditRating IN ('B', 'C') OR Volatility BETWEEN 0.10 AND 0.20 THEN 'Medium Risk'
+    ELSE 'High Risk'
+  END AS RiskScore
+FROM financial_instruments;
 
