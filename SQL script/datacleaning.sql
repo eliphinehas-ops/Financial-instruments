4136b78abb38b8501024a47d70fae833f1b4fd8a
eliphinehas-ops <eliphinehas@gmail.com>
2025-10-26 22:31:44 +0530

    Add data cleaning query for financial instruments
    Added a SQL query to clean and standardize financial instrument data.

--- SQL script/datacleaning.sql
+++ SQL script/datacleaning.sql
@@ -1,1 +1,15 @@
+-- Clean and standardize financial instrument data
+SELECT
+  InstrumentID,
+  UPPER(InstrumentType) AS InstrumentType,
+  UPPER(Issuer) AS Issuer,
+  UPPER(Sector) AS Sector,
+  CreditRating,
+  MarketValue,
+  ROUND(Volatility, 2) AS Volatility,
+  CASE
+    WHEN ComplianceFlag = 'Yes' THEN 'Compliant'
+    ELSE 'Non-Compliant'
+  END AS ComplianceStatus
+FROM financial_instruments;
 
